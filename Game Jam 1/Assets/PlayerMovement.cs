b6f4f4c7406c21a81d9668604ac506d23dfe22fd
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2021-02-28 20:20:22 +0700

    no message

---
+++ Game Jam 1/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; //Speed of Player
+    public Rigidbody2D rb;
+    public Camera cam;
+    Vector2 movement; 
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    
+        Vector2 lookDir = mousePos - rb.position; //gets direction from player to mouse
+        float angle = Mathf.Atan2(lookDir.y , lookDir.x) * Mathf.Rad2Deg - 90f; //remove the 90 if he is rotated 90 degrees too far in one direction, this was just from the tutorial i was using 
+        rb.rotation = angle;
+    }
+}
